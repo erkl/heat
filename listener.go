0c547692b81d819c13b5758b1109a76bdd90398c
Erik Lundin <erik@kvick.co>
2015-02-21 14:40:03 +0000

    Define and implement the Listener interface

---
+++ listener.go
@@ -0,0 +1,38 @@
+package wire
+
+import (
+	"net"
+)
+
+type Listener interface {
+	// Accept waits for and returns the next connection to the listener.
+	Accept() (Conn, error)
+
+	// Close closes the listener.
+	Close() error
+
+	// Addr returns the listener's network address.
+	Addr() net.Addr
+}
+
+type xListener struct {
+	net.Listener
+}
+
+func (l *xListener) Accept() (Conn, error) {
+	conn, err := l.Listener.Accept()
+	if err != nil {
+		return nil, err
+	}
+
+	return newConn(conn), nil
+}
+
+func ListenTCP(addr string) (Listener, error) {
+	ln, err := net.Listen("tcp", addr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &xListener{ln}, nil
+}
