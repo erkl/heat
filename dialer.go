26ea633126fb163ca7c923a3866ed9aa6776b2af
Erik Lundin <erik@kvick.co>
2015-02-21 00:00:33 +0000

    Move Conn out of dialer.go and into conn.go

--- dialer.go
+++ dialer.go
@@ -3,24 +3,12 @@ package wire
 import (
 	"crypto/tls"
 	"net"
-	"sync"
 	"time"
-
-	"github.com/erkl/xo"
 )
 
 type Dialer interface {
 	DialTCP(addr string) (Conn, error)
 	DialTLS(addr string) (Conn, error)
-}
-
-type Conn interface {
-	xo.ReadWriter
-
-	// Close closes the connection. The recycle parameters should be true if
-	// the last request-response cycle terminated cleanly, and the connection
-	// can be reused (at the Dialer's discretion).
-	Close(recycle bool)
 }
 
 type xDialer struct {
@@ -52,41 +40,4 @@ func (d *xDialer) DialTLS(addr string) (Conn, error) {
 	}
 
 	return newConn(conn), nil
-}
-
-// Pool of buffers used by xConn instances.
-var bufpool = &sync.Pool{
-	New: func() interface{} {
-		buf := make([]byte, 8192)
-		return [2][]byte{buf[:4096], buf[4096:]}
-	},
-}
-
-type xConn struct {
-	// The underlying net.Conn.
-	conn net.Conn
-
-	// Embedded buffered reader/writer.
-	xo.Reader
-	xo.Writer
-
-	// Read/write buffers, returned to bufpool after the connection
-	// is closed.
-	bufs [2][]byte
-}
-
-func newConn(conn net.Conn) *xConn {
-	bufs := bufpool.Get().([2][]byte)
-
-	return &xConn{
-		conn:   conn,
-		Reader: xo.NewReader(conn, bufs[0]),
-		Writer: xo.NewWriter(conn, bufs[1]),
-		bufs:   bufs,
-	}
-}
-
-func (c *xConn) Close(recycle bool) {
-	bufpool.Put(c.bufs)
-	c.conn.Close()
 }
